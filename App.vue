9295d9be7fdfa8b827ea5f567e45a04c336bc34e
wfwy <liuwang06@163.com>
2020-03-07 20:54:09 +0800

    增加首页横向滑动模块

--- App.vue
+++ App.vue
@@ -14,4 +14,13 @@
 
 <style>
 	/*每个页面公共css */
+	.page {
+		width: 100%;
+		height: 100%;
+		background-color: #f7f7f7;		
+	}
+	
+	.page-block {
+		background-color: #ffffff;
+	}
 </style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,16 +1,69 @@
 <template>
-	<!-- 轮播图 -->
-	<view>
-		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" class="swiper-wrapper">
+	<view class="page">
+		<!-- 轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" class="page-block swiper-wrapper">
 			<swiper-item class="swiper-item-wrapper">
-				<image src="https://static.256it.com/wp-blog/swiper/it_dl.jpg" class="swiper-item-img"></image>
-				<text class="swiper-item-text">轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称</text>
+				<image src="https://static.256it.com/wp-blog/swiper/it_dl.jpg"></image>
+				<text>轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称</text>
 			</swiper-item>
 			<swiper-item class="swiper-item-wrapper">
 				<image src="https://static.256it.com/wp-blog/cover/idea.jpg" class="swiper-item-img"></image>
 				<text class="swiper-item-text">轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称轮播图的文章名称</text>
 			</swiper-item>
 		</swiper>
+		
+		<!-- 精选栏目 -->
+		<view class="page-block selected-wrapper">
+			<view class='selected-subtitle'>
+				<view class='selected-subtitle-left'>精选</view>
+				<view class='selected-subtitle-right'>滑动查看</view>
+			</view>
+			
+			<scroll-view scroll-x="true" class="selected-nav">
+				<view class="selected-nav-item-wrapper">
+					<view class="selected-nav-item">
+						<image src="../../static/images/uploads/wp-blog.png"></image>
+						<text>
+							官网
+						</text>
+					</view>
+				</view>
+				<view class="selected-nav-item-wrapper">
+					<view class="selected-nav-item">
+						<image src="../../static/images/uploads/search.jpg"></image>
+						<text>
+							搜索
+						</text>
+					</view>
+				</view>
+				<view class="selected-nav-item-wrapper">
+					<view class="selected-nav-item">
+						<image src="../../static/images/uploads/comment.png"></image>
+						<text>
+							留言板
+						</text>
+					</view>
+				</view>
+				<view class="selected-nav-item-wrapper">
+					<view class="selected-nav-item">
+						<image src="../../static/images/uploads/rankings.jpg"></image>
+						<text>
+							排行
+						</text>
+					</view>
+				</view>
+				<view class="selected-nav-item-wrapper">
+					<view class="selected-nav-item">
+						<image src="../../static/images/uploads/miniprogram.png"></image>
+						<text>
+							小程序
+						</text>
+					</view>
+				</view>
+			</scroll-view>			
+		</view>
+		
+		
 	</view>
 </template>
 
