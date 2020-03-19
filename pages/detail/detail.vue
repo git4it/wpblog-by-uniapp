47b7c49eae6b7b57b38545e4ad988884e5026de9
wfwy <liuwang06@163.com>
2020-03-19 23:22:46 +0800

    文章详情样式

--- pages/detail/detail.vue
+++ pages/detail/detail.vue
@@ -1,33 +1,58 @@
-<template>
-	<view>
-		<view class="detail-content">
-			<view class="post-title">postId:{{postId}}</view>
-			<view class="post-data"></view>
-			<view class="post-summary"></view>
+<template style="height: 100%;">
+	<view class="page">
+		<view class="page-block post-content">
+			<view class="post-title">{{detail.title.rendered}}</view>
+			<view class="post-data">
+				<image src="../../static/images/calendar.png"></image>
+				<text>{{detail.post_date}}</text>
+				<image src="../../static/images/comments.png"></image>
+				<text>{{detail.total_comments}}</text>
+				<image src="../../static/images/home-like.png"></image>
+				<text>{{detail.like_count}}</text>
+				<image src="../../static/images/pageviews.png"></image>
+				<text>{{detail.pageviews}}</text>
+			</view>
+			<view class="post-summary" v-html="detail.content.rendered"></view>
 		</view>
+		<copyright></copyright>
 	</view>
 </template>
 
 <script>
+	import copyright from '../../components/copyright.vue';
+	
 	export default {
 		data() {
 			return {
-				postId:''
+				detail:{}
 			}
 		},
 		onLoad(params) {
 			let me = this;
-			// 获取上一个页面传入的参数
-			let postId = params.postId;
-			console.log(postId);
-			me.postId = postId;
+			me.getPost(params);
 		},
 		methods: {
-			
+			getPost(params) {
+				let me = this;
+				let $config = me.$config;
+				uni.request({
+					url:$config.baseUrl + '/posts/' + params.postId,
+					method:"GET",
+					success:(res) => {
+						console.log(res);
+						if (res.statusCode === 200) {
+							me.detail = res.data;
+						}
+					}
+				})
+			}
+		},
+		components:{
+			copyright
 		}
 	}
 </script>
 
 <style>
-
+	@import url("detail.css");
 </style>
